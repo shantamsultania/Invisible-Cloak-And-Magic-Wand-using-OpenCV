be69c6c10faf96bca0daaac9a93d2e079539e26d
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2020-05-28 17:56:36 +0530

    Update invi.py

--- invi.py
+++ invi.py
@@ -9,10 +9,7 @@ time.sleep(1)
 count = 0
 background = 0
 
-# capturing the background in range of 60
-# you should have video that have some seconds
-# dedicated to background frame so that it
-# could easily save the background image
+#just leave the screen up and running for like 5 sec ith just background 
 for i in range(60):
     return_val, background = capture_video.read()
     if return_val == False:
@@ -20,7 +17,6 @@ for i in range(60):
 
 background = np.flip(background, axis=1)  # flipping of the frame
 
-# we are reading from video
 while (capture_video.isOpened()):
     return_val, img = capture_video.read()
     if not return_val:
@@ -29,14 +25,7 @@ while (capture_video.isOpened()):
     img = np.flip(img, axis=1)
 
     # convert the image - BGR to HSV
-    # as we focused on detection of red color
-
-    # converting BGR to HSV for better
-    # detection or you can convert it to gray
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # ranges should be carefully chosen
-    # setting the lower and upper range for mask1
     lower_red = np.array([100, 40, 40])
     upper_red = np.array([100, 255, 255])
     mask1 = cv2.inRange(hsv, lower_red, upper_red)
@@ -47,6 +36,7 @@ while (capture_video.isOpened()):
 
     # the above block of code could be replaced with
     # some other code depending upon the color of your cloth
+    # i am using red you can use any R G B colour
     mask1 = mask1 + mask2
 
     # Refining the mask corresponding to the detected red color
@@ -59,7 +49,7 @@ while (capture_video.isOpened()):
     res2 = cv2.bitwise_and(img, img, mask=mask2)
     final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
 
-    cv2.imshow("INVISIBLE MAN", final_output)
+    cv2.imshow("INVISIBLE CLOAK", final_output)
     k = cv2.waitKey(10)
     if k == 27:
         break
